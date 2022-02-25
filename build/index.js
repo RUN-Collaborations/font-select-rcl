a811a6d88fe77d6707edd0353837d0fcdc916f23
klappy <christopher@klapp.name>
2022-02-25 15:52:08 -0500

    fix build for npmjs

---
+++ build/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "FontOption", {
+  enumerable: true,
+  get: function get() {
+    return _FontOption["default"];
+  }
+});
+Object.defineProperty(exports, "FontOptions", {
+  enumerable: true,
+  get: function get() {
+    return _FontOptions["default"];
+  }
+});
+
+var _FontOption = _interopRequireDefault(require("./components/FontOption"));
+
+var _FontOptions = _interopRequireDefault(require("./components/FontOptions"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import FontOption from './components/FontOption';
+import FontOptions from './components/FontOptions';
+
+export {
+  FontOption,
+  FontOptions,
+};
