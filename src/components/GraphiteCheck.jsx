9f4c5f003e9801c2581256fbe73ca37f27eb85f5
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2022-03-03 21:12:14 -0600

    Allow override of GraphiteCheck

--- src/components/GraphiteCheck.jsx
+++ src/components/GraphiteCheck.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
-export default function GraphiteCheck({ testClient }){
+export default function GraphiteCheck({ testClient, alwaysUse }){
 
   const graphiteCheckComponent = (navigator.userAgent.toLowerCase().indexOf(testClient) > -1);
+  
+  const displayGraphiteFonts = (alwaysUse || graphiteCheckComponent);
 
-  return (<div>{graphiteCheckComponent ? "Firefox detected. Graphite-enabled fonts will be displayed" : "Firefox not detected. Graphite-enabled fonts will not be displayed"}</div>);
+  const proper = useCallback((str) => {
+    return str[0].toUpperCase() + str.slice(1);
+  }, []);
+
+  const properClient = useMemo(() => proper(testClient), [proper]);
+
+
+  return (<div>{properClient} {graphiteCheckComponent ? " detected." : " not detected."}{' '}
+  {alwaysUse ? "Ignoring environment test." : "Applying environment test."}{' '}
+  {displayGraphiteFonts ? "Graphite-enabled fonts will be presented. They will render properly if Graphite is enabled." : "Graphite-enabled fonts will not be displayed."}</div>);
 };
 
 GraphiteCheck.propTypes = {
   /** name of browser to test */
   name: PropTypes.string,
+  /** skip environment test */
+  alwaysUse: PropTypes.bool,
 };
 
 GraphiteCheck.propDefaults = {
   testClient: 'firefox',
+  alwaysUse: false,
 };
 
--- src/components/GraphiteCheck.md
+++ src/components/GraphiteCheck.md
@@ -3,7 +3,7 @@
 ```jsx
 /** Should Graphite-enabled fonts be displayed? */
 /** This checks for Firefox and assumes gfx.font_rendering.graphite.enabled is set to true in about:config. */
-const graphiteCheck = { testClient: 'firefox' };
+const graphiteCheck = { testClient: 'firefox', alwaysUse: false };
 
 <GraphiteCheck {...graphiteCheck} ></GraphiteCheck>;
 ```
